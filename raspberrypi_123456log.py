a2b426d96ca25c6a7676bca6f69ea6c65c5e3d45
Aravindreddyb <66541599+Aravindreddyb@users.noreply.github.com>
2020-08-08 17:46:00 +0530

    Add files via upload

---
+++ raspberrypi_123456log.py
@@ -0,0 +1,7 @@
+2020-08-6 08:55:07,986   ibmiotf.device.Client      INFO    Connected successfully: d:cef01x:raspberrypi:123456
+
+2020-08-6 11:37:05,547   ibmiotf.device.Client      INFO    Connected successfully: d:cef01x:raspberrypi:123456
+2020-08-7 13:33:59,391   ibmiotf.device.Client      INFO    Connected successfully: d:cef01x:raspberrypi:123456
+2020-08-7 15:56:32,762   ibmiotf.device.Client      INFO    Connected successfully: d:cef01x:raspberrypi:123456
+2020-08-8 18:58:01,010   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-08-8 19:58:05,624   ibmiotf.device.Client      INFO    Connected successfully: d:cef01x:raspberrypi:123456
